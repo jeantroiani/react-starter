474f449954f2be2326275430d1d1917ad3a27f11
Simon Søllingvraa <simsoll@users.noreply.github.com>
2016-06-14 21:32:08 +0200

    Started implementing a manage course page

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,14 +1,33 @@
 import * as types from './actionTypes';
-import courseApi from '../api/mockCourseApi';
+import CourseApi from '../api/mockCourseApi';
 
 export function loadCoursesSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function createCourseSuccess(courses) {
+    return { type: types.CREATE_COURSE_SUCCESS, courses};
+}
+
+export function updateCourseSuccess(courses) {
+    return { type: types.UPDATE_COURSE_SUCCESS, courses};
+}
+
 export function loadCourses() {
     return function(dispatch) {
-        return courseApi.getAllCourses().then(courses => {
+        return CourseApi.getAllCourses().then(courses => {
             dispatch(loadCoursesSuccess(courses));
+        }).catch(error => {
+            throw(error);
+        });
+    };
+}
+
+export function saveCourse(course) {
+    return function (dispatch, getState) {
+        return CourseApi.saveCourse(course).then(savedCourse => {
+            course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+            dispatch(createCourseSuccess(saveCourse));
         }).catch(error => {
             throw(error);
         });
---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,72 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
+
+class ManageCoursePage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            course: Object.assign({}, props.course),
+            errors: {}
+        };
+
+        this.updateCourseState = this.updateCourseState.bind(this);
+    }
+
+    updateCourseState(event) {
+        const field = event.target.name;
+        let course = this.state.course;
+        course[field] = event.target.value;
+        return this.setState({course: course})
+    }
+
+    render() {
+        return (
+            <CourseForm 
+                allAuthors={this.props.authors}
+                onChange={this.updateCourseState}
+                course={this.state.course}
+                errors={this.state.errors}
+            />
+        );
+    }
+}
+
+ManageCoursePage.propTypes = {
+    authors: PropTypes.array.isRequired,
+    course: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+    let course = {
+        id: '',
+        watchHref: '',
+        title: '',
+        authorId: '',
+        length: '',
+        category: ''
+    };
+
+    const authorsFormattedForDropdown = state.authors.map(author => {
+        return {
+            value: author.id,
+            text: author.firstName + ' ' + author.lastName
+        };
+    });
+
+    return {
+        authors: authorsFormattedForDropdown,
+        course: course
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
